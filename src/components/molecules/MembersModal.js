9deda0ced4bcb10378f50d0460c47cae2ed2bdc8
Rafaelle <66576007+Belorgey@users.noreply.github.com>
2022-03-29 14:36:52 +0200

    Petit nettoyage et petites corrections de ListDetailsScreen et MembersModal

--- src/components/molecules/MembersModal.js
+++ src/components/molecules/MembersModal.js
@@ -5,53 +5,25 @@ import { DatabaseContext } from '../../navigation/DatabaseProvider';
 import { Colors } from '../../styles';
 import MembersButton from '../atoms/MembersButton';
 import FormInput from '../atoms/FormInput';
-import FormButton from '../atoms/FormButton';
 import ListButton from '../atoms/ListButton';
 
-const MembersModal = ({listUid, listName, listOwner, userId}) => {
+const MembersModal = ({ listUid }) => {
   const [modalVisible, setModalVisible] = useState(false);
 
-  const {
-    addListMember,
-    deleteItemInList,
-    deleteList,
-    deletion,
-    getItemsList,
-    getListDetails,
-    getListMembers,
-    getUserByEmail,
-    itemList,
-    listDetails,
-    listMembers,
-    setDeletion,
-    setUserToShare,
-    updateInCart,
-    updateListName,
-    userData,
-    userToShare } = useContext(DatabaseContext);
+  const { addListMember, getListMembers, listMembers } = useContext(DatabaseContext);
+  const { getListDetails, listDetails } = useContext(DatabaseContext);
+  const { getUserByEmail, setUserToShare, userToShare } = useContext(DatabaseContext);
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [items, setItems] = useState({});
 
-  const { user, logout } = useContext(AuthContext);
-  
-  const handleNameChange = (textInput) => {
-    setName(textInput)
-  }
   const handleEmailChange = (textInput) => {
     setEmail(textInput)
   }
 
-  const getItems = () => {
-    listDetails.map((list) => (
-      setItems(list["itemsList"])
-    ))
-  }
-
   //read
   useEffect(() => {
-    getListDetails(listUid)
     getListMembers(listUid)
   }, []);
 
@@ -62,20 +34,17 @@ const MembersModal = ({listUid, listName, listOwner, userId}) => {
 
   const checkIfUserIsNew = (user) => {
     let alreadyMember = false
-    getListMembers(listUid)
     listMembers.map((member) => {
-      console.log(member)
-      console.log(user)
-      if (member.userId === user.uid) {
-        alreadyMember = true
+      if (member.userId===user.uid) {
+        alreadyMember=true
       }
     });
     if (alreadyMember) {
       alert("Cette personne est déjà dans la liste")
     }
     else {
-      addListMember(listUid, user.uid, user.email)
-      alert("Ajout de " + user.email)
+      addListMember(listUid,user.uid,user.email)
+      alert("Ajout de "+user.email)
     }
     setUserToShare([])
   }
@@ -96,24 +65,24 @@ const MembersModal = ({listUid, listName, listOwner, userId}) => {
         }}
       >
         <View style={styles.modalView}>
-        <View key="Membres">
-          <Text style={styles.textStyle}>Membres de la liste</Text>
-        </View>
-        {listMembers.map((member) => (
-          <View key={member.userId}>
-            <Text style={styles.modalText} key={"Id" + member.userId}>{member.email}</Text>
+          <View key="Membres">
+            <Text style={styles.textStyle}>Membres de la liste</Text>
           </View>
-        ))}
-        <View>
-          <FormInput
-            value={email}
-            placeholderText="Email de votre ami"
-            onChangeText={handleEmailChange}
-            style={styles.input}
-            key="AddUserByEmail"
-          />
-          <ListButton buttonTitle={"Partager"} onPress={() => handleShareList(email)} key={"ShareButton"} />
-        </View>
+          {listMembers.map((member) => (
+            <View key={member.userId}>
+              <Text style={styles.modalText} key={"Id" + member.userId}>{member.email}</Text>
+            </View>
+          ))}
+          <View>
+            <FormInput
+              value={email}
+              placeholderText="Email de votre ami"
+              onChangeText={handleEmailChange}
+              style={styles.input}
+              key="AddUserByEmail"
+            />
+            <ListButton buttonTitle={"Partager"} onPress={() => handleShareList(email)} key={"ShareButton"} />
+          </View>
         </View>
       </Modal>
       <MembersButton style={styles.membersButton} onPress={() => setModalVisible(true)} />
@@ -164,7 +133,7 @@ const styles = StyleSheet.create({
   modalText: {
     marginBottom: 15,
     textAlign: "center",
-    fontSize:17,
+    fontSize: 17,
     zIndex: 2
   },
   membersButton: {
@@ -177,8 +146,8 @@ const styles = StyleSheet.create({
     // width: '90%',
     // backgroundColor:Colors.WHITE,
     borderRadius: 5,
-    marginBottom:20,
-    marginTop:25,
+    marginBottom: 20,
+    marginTop: 25,
   },
   list: {
     justifyContent: 'center',
