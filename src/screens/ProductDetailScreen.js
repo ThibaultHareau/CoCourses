52516ab39cecefa0f6ddbcb3a5c8060e5af465b9
ThibaultHareau <hareaut@gmail.com>
2022-03-30 00:29:40 +0200

    Ajout des nouvelles caractéristiques d'un produit

--- src/screens/ProductDetailScreen.js
+++ src/screens/ProductDetailScreen.js
@@ -83,8 +83,14 @@ export default function ProductDetailScreen ( {navigation, route} ) {
             {(imageUrl===null) ? null :<Image style={{width:windowWidth,height:200}} source={{uri : imageUrl}} />}
           </View> 
           <View key={item.uuid}>
-            <Text style={styles.listName} key={"Name"+item.uuid}>{item.name}</Text>
-            <Text key={"Text"+item.uuid} style={styles.listDesc}>{item.text}</Text>
+            <View key="TitleBlock" style={styles.titleBlock}>
+              <Text style={styles.listName} key={"Name"+item.uuid}>{item.name}</Text>
+            </View>
+            <View key="DescriptionBlock" style={styles.descriptionBlock}>
+              {("text" in item )? <Text key={"Text"+item.uuid} style={styles.listDesc}>Ce qu'on en dit : {item.text}</Text> : null}
+              {("bilanCarbone" in item )? <Text key={"blianCarbone"+item.uuid} style={styles.listDesc}>Bilan Carbone : {item.bilanCarbone}</Text> : null}
+              {("origine" in item )? <Text key={"origine"+item.uuid} style={styles.listDesc}>Origine : {item.origine}</Text> : null}
+            </View>
             {/* <FormButton buttonTitle='Supprimer' onPress={() => handleListDelete(list)} key={"delete"+list.uuid}/> */}
             {/* <FormButton buttonTitle='Modifier' onPress={() => setOnEdit(list)} key={"update"+list.uuid}/> */}
             {/* <FormButton buttonTitle='Ajouter' onPress={() => navigation.navigate("ListsChoice",{itemName:list.name,itemId:list.uuid,userId:userId,deptId:deptId})} key={"add"+list.uuid}/> */}
@@ -109,6 +115,12 @@ export default function ProductDetailScreen ( {navigation, route} ) {
       alignItems: 'center',
       zIndex:0
     },
+    descriptionBlock: {
+      marginTop:"2%",
+      marginLeft:"2%",
+      width:"96%",
+      justifyContent:"center"
+    },
     input: {
       padding:10,
       borderWidth:1,
@@ -123,12 +135,12 @@ export default function ProductDetailScreen ( {navigation, route} ) {
       paddingRight: 15,
       borderRadius: 5
     },
-    listName: {
+    listName: {      
       fontSize: 28,
-      top:50
+      marginTop:"5%",
+      textAlign: 'center'
     },
     listDesc: {
-      top:70
     },
     mainFrame: {
       position:"absolute",
@@ -139,5 +151,9 @@ export default function ProductDetailScreen ( {navigation, route} ) {
     containerModal: {
       position:'absolute',
       zIndex:2
+    },
+    titleBlock: {
+      width:"80%",
+      marginLeft:"10%",
     }
   });
